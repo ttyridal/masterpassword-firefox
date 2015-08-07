6ea92755ecdcbefdc4afe294c72b5a00fcaf4a14
Torbjorn Tyridal <torbjorn@tyridal.no>
2015-08-07 22:56:12 +0200

    some whitespace doesn't hurt

--- ext/data/main_popup.js
+++ ext/data/main_popup.js
@@ -36,7 +36,7 @@ var session_store={};
 function recalculate(hide_after_copy, retry) {
     $('#thepassword').html('(calculating..)');
     $('#usermessage').html("Please wait...");
-    if ($('#sitename').val()==null || $('#sitename').val()=="") {
+    if ($('#sitename').val() == null || $('#sitename').val() == "") {
         $('#usermessage').html("need sitename");
         return;
     }
@@ -44,84 +44,107 @@ function recalculate(hide_after_copy, retry) {
     if (!mpw_session) {
         try {
             mpw_session = mpw(
-            session_store.username,
-            session_store.masterkey);
+                session_store.username,
+                session_store.masterkey);
         } catch(err)
         {
             if (retry) {
                 $('#usermessage').html("Waiting didn't help :(");
                 $('#thepassword').html('(Failed)');
-                console.log(err.message,"\n",err.stack);
+                console.log(err.message, "\n", err.stack);
             } else {
                 $('#usermessage').html("waiting for asm.js");
-                setTimeout(function(){ recalculate(hide_after_copy, true); }, 300);
+                setTimeout(
+                        function(){ recalculate(hide_after_copy, true); },
+                        300);
             }
             return;
         }
     }
 
-    console.log("calc password "+$('#sitename').val()+" . "+parseInt($('#passwdgeneration').val())+" . "+$('#passwdtype').val());
-    var i,s="",pass=mpw_session($('#sitename').val(), parseInt($('#passwdgeneration').val()), $('#passwdtype').val()),
-        verify_pass=mpw_session(".", 0, "n");
-        for (i=0;i<pass.length;i++)s+="&middot;";
+    console.log("calc password " +
+                $('#sitename').val() +
+                " . " +
+                parseInt($('#passwdgeneration').val()) +
+                " . " +
+                $('#passwdtype').val());
 
-        $('#verify_pass_fld').html("Verify: "+verify_pass);
-        $('#thepassword').html('<a href="" id="showpass">'+s+'</a>');
-        $('#thepassword').attr('data-pass',pass);
+    var i,
+        s = "",
+        pass = mpw_session(
+                 $('#sitename').val(),
+                 parseInt($('#passwdgeneration').val()),
+                 $('#passwdtype').val()),
+        verify_pass = mpw_session(".", 0, "n");
 
-        copy_to_clipboard("text/plain",pass);
+
+        for (i = 0; i < pass.length; i++)
+            s += "&middot;";
+
+        $('#verify_pass_fld').html("Verify: " + verify_pass);
+        $('#thepassword').html('<a href="" id="showpass">' + s + '</a>');
+        $('#thepassword').attr('data-pass', pass);
+
+        copy_to_clipboard("text/plain", pass);
         update_page_password_input(pass);
         if (hide_after_copy) {
             addon.port.emit('close');
         }
-        $('#usermessage').html("Password for "+$('#sitename').val()+" copied to clipboard");
+        $('#usermessage').html("Password for " + $('#sitename').val() + " copied to clipboard");
 }
 
 function update_with_settings_for(domain) {
-    var first=true;
-    if (session_store['sites']===undefined) return;
-    if (session_store.sites[domain]===undefined) return;
+    var first = true;
 
-    $.each(session_store.sites[domain], function(key,val) {
-        $('#storedids').append('<option>'+key);
+    if (session_store['sites'] === undefined) return;
+    if (session_store.sites[domain] === undefined) return;
+
+    $.each(session_store.sites[domain], function(key, val) {
+        $('#storedids').append('<option>' + key);
         if (first) {
             $('#sitename').val(key);
             $('#passwdgeneration').val(val.generation);
             $('#passwdtype').val(val.type);
-            first=false;
+            first = false;
         } else
             $('#storedids_dropdown').show();
     });
 }
 
-function popup(session_store_,opened_by_hotkey) {
-    var recalc=false;
+function popup(session_store_, opened_by_hotkey) {
+    var recalc = false;
+
     session_store = session_store_;
-    if (session_store.username==null || session_store.masterkey==null) {
+    if (session_store.username == null || session_store.masterkey == null) {
         $('#main').hide();
         $('#logoutbtn').hide();
         $('#sessionsetup').show();
-        mpw_session=null;
-        if (session_store.username==null)
-            window.setTimeout(function(){$('#username').focus();},0.1);
+        mpw_session = null;
+        if (session_store.username == null)
+            window.setTimeout(
+                    function(){$('#username').focus();},
+                    0.1);
         else {
             $('#username').val(session_store.username);
-            window.setTimeout(function(){$('#masterkey').focus();},0.1);
+            window.setTimeout(
+                    function(){$('#masterkey').focus();},
+                    0.1);
         }
     } else {
-        recalc=true;
+        recalc = true;
         $('#logoutbtn').show();
         $('#main').show();
     }
 
     get_active_tab_url().then(function(url){
         var domain = parse_uri(url)['domain'].split("."),
-            significant_parts=2;
-        if (domain.length>2 && domain[domain.length-2].toLowerCase()=="co")
-            significant_parts=3;
-        while(domain.length>1 && domain.length>significant_parts)domain.shift();
-        domain=domain.join(".");
-        $('.domain').attr('value',domain);
+            significant_parts = 2;
+        if (domain.length > 2 && domain[domain.length-2].toLowerCase() == "co")
+            significant_parts = 3;
+        while(domain.length > 1 && domain.length > significant_parts)
+        domain.shift();
+        domain = domain.join(".");
+        $('.domain').attr('value', domain);
         update_with_settings_for(domain);
         if(recalc) {
             recalculate(opened_by_hotkey);
@@ -154,20 +177,20 @@ $('#sessionsetup > form').on('submit', function(){
 });
 
 $('#logoutbtn').on('click',function(){
-    session_store.masterkey=null;
+    session_store.masterkey = null;
     addon.port.emit('store_update', session_store);
     popup(session_store);
     $('#usermessage').html("session destroyed");
 });
 
-$('#generatepassword').on('click', function(){
+$('#generatepassword').on('click', function(){});
 
-});
 $('#siteconfig_show').on('click', function(){
     $('#siteconfig').show();
     $(this).hide();
     return false;
 });
+
 $('#thepassword').on('click', '#showpass', function(e){
     var $t = $(this.parentNode);
     $t.html( $t.attr('data-pass') );
@@ -175,7 +198,8 @@ $('#thepassword').on('click', '#showpass', function(e){
 });
 
 $('#storedids_dropdown').on('click', function(e){
-    var sids=$('#storedids');
+    var sids = $('#storedids');
+
     if (sids.is(":visible"))
         sids.hide();
     else {
@@ -183,9 +207,11 @@ $('#storedids_dropdown').on('click', function(e){
         sids.focus();
     }
 });
+
 $('#storedids').on('change', function(){
     var site = $(this).val(),
         domain = $('#domain').val();
+
     $('#sitename').val(site);
     $('#passwdgeneration').val(session_store.sites[domain][site].generation);
     $('#passwdtype').val(session_store.sites[domain][site].type);
@@ -195,10 +221,11 @@ $('#storedids').on('change', function(){
 
 function save_site_changes_and_recalc(){
     var domain = $('#domain').val();
-    if (session_store['sites']===undefined)
-        session_store.sites={};
-    if (session_store.sites[domain]===undefined)
-        session_store.sites[domain]={};
+
+    if (session_store['sites'] === undefined)
+        session_store.sites = {};
+    if (session_store.sites[domain] === undefined)
+        session_store.sites[domain] = {};
 
     session_store.sites[domain][$('#sitename').val()] = {
         generation:$('#passwdgeneration').val(),
@@ -210,8 +237,8 @@ function save_site_changes_and_recalc(){
     recalculate();
 }
 
-$('#siteconfig').on('change','select,input',save_site_changes_and_recalc);
-$('#sitename').on('change',save_site_changes_and_recalc);
+$('#siteconfig').on('change', 'select,input', save_site_changes_and_recalc);
+$('#sitename').on('change', save_site_changes_and_recalc);
 
 $('#configbtn').on('click',function(){
     addon.port.emit('openconfig');
