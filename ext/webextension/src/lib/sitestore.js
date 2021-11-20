ba73c023665937f21e4b1a56d7da2e691fe9f405
Torbjorn Tyridal <ttyridal@users.noreply.github.com>
2021-11-20 19:37:03 +0100

    whitespace/formating

--- ext/webextension/src/lib/sitestore.js
+++ ext/webextension/src/lib/sitestore.js
@@ -34,130 +34,130 @@ export class NeedUpgradeError extends Error {
 }
 
 export class SiteStore {
-constructor(store) {
-    this._needs_upgrade = false;
-    this.store = store;
-}
+    constructor(store) {
+        this._needs_upgrade = false;
+        this.store = store;
+    }
 
-need_upgrade() {
-    return this._needs_upgrade;
-}
+    need_upgrade() {
+        return this._needs_upgrade;
+    }
 
-get() {
-    return this.get_nowrap().then(sites => sites.map(e => new Site(e)));
-}
+    get() {
+        return this.get_nowrap().then(sites => sites.map(e => new Site(e)));
+    }
 
-get_nowrap() {
-    return new Promise(resolve => {
-        this.store.get(['sites', 'sitedata'], d => {
-            if ('sitedata' in d)
-                resolve(d.sitedata);
-            else if ('sites' in d) {
-                this._needs_upgrade = true;
-                let result = [];
-                for (const [domain, sitedict] of Object.entries(d.sites)) {
-                    for (const [sitename, props] of Object.entries(sitedict)) {
-                        props.sitename = sitename;
-                        props.url = [domain];
-                        result.push(props);
+    get_nowrap() {
+        return new Promise(resolve => {
+            this.store.get(['sites', 'sitedata'], d => {
+                if ('sitedata' in d)
+                    resolve(d.sitedata);
+                else if ('sites' in d) {
+                    this._needs_upgrade = true;
+                    let result = [];
+                    for (const [domain, sitedict] of Object.entries(d.sites)) {
+                        for (const [sitename, props] of Object.entries(sitedict)) {
+                            props.sitename = sitename;
+                            props.url = [domain];
+                            result.push(props);
+                        }
                     }
+                    resolve(result);
+                } else
+                    resolve([]);
+            });
+        });
+    }
+
+    addOrReplace(site) {
+        if (this._needs_upgrade) {
+            console.error("need upgrade before addOrReplace");
+            throw new NeedUpgradeError();
+        }
+        return new Promise(resolve => {
+            this.get()
+            .then(sites=>{
+                const siteidx = sites.findIndex(e => e.sitename == site.sitename);
+                console.log("addOrReplace", site.sitename, siteidx);
+                if (siteidx == -1)
+                    sites.push(site)
+                else
+                    sites[siteidx] = site;
+
+                this.store.set({'sitedata': sites}, ()=>resolve());
+            })
+        });
+    }
+
+    set(sites) {
+        return new Promise(resolve => {
+            this.store.set({'sitedata': sites}, ()=>resolve());
+        });
+    }
+
+    update(sitename, params) {
+        if (this._needs_upgrade) {
+            console.error("need upgrade before update");
+            throw new NeedUpgradeError();
+        }
+        return new Promise((resolve, fail) => {
+            this.get()
+            .then(sites=>{
+                const siteidx = sites.findIndex(e => e.sitename == sitename);
+                if (siteidx == -1) {
+                    fail(new Error("Not found"));
+                    return;
                 }
-                resolve(result);
-            } else
-                resolve([]);
+
+                Object.assign(sites[siteidx], params);
+                this.store.set({'sitedata': sites}, ()=>resolve());
+            })
         });
-    });
-}
-
-addOrReplace(site) {
-    if (this._needs_upgrade) {
-        console.error("need upgrade before addOrReplace");
-        throw new NeedUpgradeError();
     }
-    return new Promise(resolve => {
-        this.get()
-        .then(sites=>{
-            const siteidx = sites.findIndex(e => e.sitename == site.sitename);
-            console.log("addOrReplace", site.sitename, siteidx);
-            if (siteidx == -1)
-                sites.push(site)
-            else
-                sites[siteidx] = site;
 
-            this.store.set({'sitedata': sites}, ()=>resolve());
-        })
-    });
-}
+    addurl(sitename, url) {
+        return new Promise((resolve, fail) => {
+            this.get()
+            .then(sites=>{
+                const siteidx = sites.findIndex(e => e.sitename == sitename);
+                if (siteidx == -1) {
+                    fail(new Error("Not found"));
+                    return;
+                }
 
-set(sites) {
-    return new Promise(resolve => {
-        this.store.set({'sitedata': sites}, ()=>resolve());
-    });
-}
-
-update(sitename, params) {
-    if (this._needs_upgrade) {
-        console.error("need upgrade before update");
-        throw new NeedUpgradeError();
-    }
-    return new Promise((resolve, fail) => {
-        this.get()
-        .then(sites=>{
-            const siteidx = sites.findIndex(e => e.sitename == sitename);
-            if (siteidx == -1) {
-                fail(new Error("Not found"));
-                return;
-            }
-
-            Object.assign(sites[siteidx], params);
-            this.store.set({'sitedata': sites}, ()=>resolve());
-        })
-    });
-}
-
-addurl(sitename, url) {
-    return new Promise((resolve, fail) => {
-        this.get()
-        .then(sites=>{
-            const siteidx = sites.findIndex(e => e.sitename == sitename);
-            if (siteidx == -1) {
-                fail(new Error("Not found"));
-                return;
-            }
-
-            let urls = new Set(sites[siteidx].url);
-            urls.add(url);
-            sites[siteidx].url = Array.from(urls);
-            this.store.set({'sitedata': sites}, ()=>resolve());
-        });
-    });
-}
-
-remove(sitename, url) {
-    if (this._needs_upgrade) {
-        console.error("need upgrade before remove");
-        throw new NeedUpgradeError();
-    }
-    return new Promise((resolve, fail) => {
-        this.get()
-        .then(sites=>{
-            const siteidx = sites.findIndex(e => e.sitename == sitename);
-            if (siteidx == -1) {
-                fail(new Error("Not found"));
-                return;
-            }
-
-            let urls = new Set(sites[siteidx].url);
-            if (url) {
-                urls.delete(url);
+                let urls = new Set(sites[siteidx].url);
+                urls.add(url);
                 sites[siteidx].url = Array.from(urls);
-            }
-
-            if (!url || urls.size == 0)
-                sites.splice(siteidx, 1);
-
-            this.store.set({'sitedata': sites}, ()=>resolve());
+                this.store.set({'sitedata': sites}, ()=>resolve());
+            });
         });
-    });
-}
+    }
+
+    remove(sitename, url) {
+        if (this._needs_upgrade) {
+            console.error("need upgrade before remove");
+            throw new NeedUpgradeError();
+        }
+        return new Promise((resolve, fail) => {
+            this.get()
+            .then(sites=>{
+                const siteidx = sites.findIndex(e => e.sitename == sitename);
+                if (siteidx == -1) {
+                    fail(new Error("Not found"));
+                    return;
+                }
+
+                let urls = new Set(sites[siteidx].url);
+                if (url) {
+                    urls.delete(url);
+                    sites[siteidx].url = Array.from(urls);
+                }
+
+                if (!url || urls.size == 0)
+                    sites.splice(siteidx, 1);
+
+                this.store.set({'sitedata': sites}, ()=>resolve());
+            });
+        });
+    }
 }
